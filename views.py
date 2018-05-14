004f02195cd02670a61b9ed423afbfc35acb098f
RicardoAJPinto <ricardopinto1996@hotmail.com>
2018-05-14 11:31:24 +0100

    New DB Models + CRUD (Roles + Machine)

--- views.py
+++ views.py
@@ -1,30 +1,26 @@
 from app import app
 from model import *
 
-def token_required(f):  #Está fixe (TOPPPP)
+##########################      Validação Token         ##############
+
+def token_required(f):  
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
-
-            #token = token.split(' ')[1]
         if not token:
             return jsonify({'message' : 'Token is missing!'}), 401
-
         try: 
-            #print("verificar o token: '", token, "' com a chave ", app.config['SECRET_KEY'], sep='')
             data = jwt.decode(token, app.config['SECRET_KEY'])
-            #print("token verificado, user id: ", data['id'])
             current_user = User.query.filter_by(id=data['id']).first()
         except:
             return jsonify({'message' : 'Token is invalid!'}), 401
-            
-        #print('user returned: ', current_user)
         return f(current_user, *args, **kwargs)
 
     return decorated
+
+##############################################################################
 
 @app.route('/')
 def home():
@@ -41,7 +37,9 @@ def get(current_user):
     user = User.query.filter_by(id=tkn.id)       #Não trabalha como deve ser
     return jsonify({identity})
 
-@app.route('/user/<email>', methods=['DELETE'])    #Está fixe
+###############################         Delete User     #####################
+
+@app.route('/user/<email>', methods=['DELETE'])   
 @token_required
 def delete(current_user, email):
     userdel = User.query.filter_by(email=email).first()
@@ -51,7 +49,9 @@ def delete(current_user, email):
     db.session.commit()
     return jsonify({'message' : 'The user has been deleted!'})
 
-@app.route('/users', methods=['GET'])   #Está fixe
+####################################        Profile / All user
+
+@app.route('/users', methods=['GET'])   
 @token_required
 def show(current_user):
     user = User.query.all() 
@@ -65,7 +65,7 @@ def show(current_user):
 
     return jsonify({'users' : output})
     
-@app.route('/profile/<email>')      #Está fixe
+@app.route('/profile/<email>')      
 @token_required
 def profile(current_user, email):
     user = User.query.filter_by(email=email).first()
@@ -79,7 +79,9 @@ def profile(current_user, email):
 
     return jsonify({'user' : user_data})
 
-@app.route('/user123', methods=['POST']) #Está fixe
+##############################          Registo / Auth
+
+@app.route('/user123', methods=['POST']) 
 @token_required
 def create(current_user):
     data = request.get_json()
@@ -90,7 +92,7 @@ def create(current_user):
     return jsonify({'message' : 'New user created!'})
 
 
-@app.route('/login123')  #Está fixe
+@app.route('/login123')  
 def login():
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
@@ -108,12 +110,100 @@ def login():
 
     return make_response('Could not 3', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})   
 
-@app.route('/protected', methods=['GET']) #Está fixe
+
+################################    Test Token          ################################>
+
+
+@app.route('/protected', methods=['GET']) 
 @token_required
 def protected(current_user):
     token = request.headers['x-access-token']
     return jsonify(token), 200
 
+###########################             Error 404 ############################>##
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+###########################           Histórico           #########################################
+
+@app.route('/hist', methods=['POST'])
+def createhist():
+    data = request.get_json()
+    hist = Historico(owner_id=data['owner'], name=data['name'], resultado=data['resultado'])
+    db.session.add(hist) 
+    db.session.commit()  
+    return jsonify({'message' : 'New hist created!'})
+
+
+#########################           Máquina             ###########################################
+
+
+@app.route('/maquina', methods=['POST']) 
+def createmaquina():
+    data = request.get_json()
+    hist = Maquina(owner_id=data['owner'], maquina=data['maquina'])
+    db.session.add(hist) 
+    db.session.commit()  
+    return jsonify({'message' : 'New machine created!'})
+
+@app.route('/maquina', methods=['DELETE'])   
+@token_required
+def deleteMaquina(current_user):
+    data = request.get_json()
+    maq = Maquina.query.filter_by(maquina=data['maquina']).first()
+    if not maq:
+        return jsonify({'message' : 'No machine found!'})
+    db.session.delete(maq)
+    db.session.commit()
+    return jsonify({'message' : 'The machine has been deleted!'})
+
+@app.route('/addMaquina', methods=['PUT']) 
+def addMaquina():
+    data = request.get_json()
+    machine = Maquina.query.filter_by(maquina=data['mach']).first()
+    if not machine:
+        return jsonify({'message' : 'No machine found!'})
+    hist = Historico.query.filter_by(name=data['histo']).first()
+    if not hist:
+        return jsonify({'message' : 'No history found!'})
+    machine.roles.append(hist)
+    db.session.commit()
+    return jsonify({'message' : 'Machine added with history!'})
+
+
+#######################             Roles           #########################################
+
+@app.route('/role', methods=['POST'])
+def createRole():
+    data = request.get_json()
+    role = Role(name=data['name'])
+    db.session.add(role) 
+    db.session.commit()  
+    return jsonify({'message' : 'New role created!'})
+
+@app.route('/role', methods=['DELETE'])   
+@token_required
+def deleteRole(current_user):
+    data = request.get_json()
+    role = Role.query.filter_by(name=data['name']).first()
+    if not role:
+        return jsonify({'message' : 'No role found!'})
+    db.session.delete(role)
+    db.session.commit()
+    return jsonify({'message' : 'The role has been deleted!'})
+
+
+@app.route('/addRole', methods=['POST']) 
+def addRole():
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+    role = Role.query.filter_by(name=data['role']).first()
+    if not role:
+        return jsonify({'message' : 'No role found!'})
+    user.roles.append(role)
+    db.session.commit()
+    return jsonify({'message' : 'Role added to user!'})
